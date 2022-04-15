69c7044188272f2824bf22043321b3750299abce
Tomas13d <Tomas.demobio@gmail.com>
2022-04-15 04:43:53 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,53 @@
+
+
+
+function App() {
+  return (
+    <>
+    <div class="background-container">
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+        <div class="fluid"></div>
+   </div>
+   <div>
+    <h1 class="in">In Progress...</h1>  
+</div>
+</>
+  );
+}
+
+export default App;
