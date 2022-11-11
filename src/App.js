a7734e4ac3384d8b848c7978277f8c1fbcb721f1
Tomas13d <tomas.demobio@gmail.com>
2022-11-11 14:08:02 -0300

    feat: new desgin for in progress

--- src/App.js
+++ src/App.js
@@ -1,62 +1,42 @@
-import {RiWhatsappLine, RiGithubLine, RiLinkedinLine, RiInstagramLine, RiFileTextLine } from "react-icons/ri";
+import {
+  RiWhatsappLine,
+  RiGithubLine,
+  RiLinkedinLine,
+  RiInstagramLine,
+  RiFileTextLine,
+} from "react-icons/ri";
 
 function App() {
   return (
     <>
-    <div class="background-container">
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-        <div class="fluid"></div>
-   </div>
-   <div>
-   <h1 class="in">In Progress...</h1>  
-   </div>
-  
-    <div className="social-media-container">
-    <h4><u>You can reach me at</u></h4>
-            <a href="https://linkedin.com/in/tomas-demo"><RiLinkedinLine/></a>
-            <a href="https://github.com/Tomas13d"><RiGithubLine/></a>
-            <a href="https://ultramsg.com/m/3va2ZdI"><RiWhatsappLine/></a>
-            <a href="https://www.instagram.com/tomasjdemo/"><RiInstagramLine/></a><br/>
-            <a href="mailto:tomas.demobio@gmail.com">tomas.demobio@gmail.com</a>
-    </div>
-     
+      <div class="background-container">
+        <circle className="shape"></circle>
+        <circle className="shape1"></circle>
+      </div>
+      
 
-</>
+      <div className="social-media-container">
+      <h1 class="in">Hi, i'm <strong className="tomas-style">Tomas</strong></h1>
+      <p>At this moment I am updating my portfolio with new projects.</p>
+        <h4>
+          You can reach me at
+        </h4>
+        <a href="https://linkedin.com/in/tomas-demo">
+          <RiLinkedinLine className="icons"/>
+        </a>
+        <a href="https://github.com/Tomas13d" >
+          <RiGithubLine className="icons"/>
+        </a>
+        <a href="https://ultramsg.com/m/3va2ZdI">
+          <RiWhatsappLine className="icons"/>
+        </a>
+        <a href="https://www.instagram.com/tomasjdemo/">
+          <RiInstagramLine className="icons"/>
+        </a>
+        <br />
+        <a href="mailto:tomas.demobio@gmail.com">tomas.demobio@gmail.com</a>
+      </div>
+    </>
   );
 }
 
